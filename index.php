48a8ab1450bef85bdfa06b259f792f65d2b13644
Morad LABRID <labrid.morad@hotmail.com>
2022-03-29 13:07:15 +0200

    first commit navbar

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
-    
+  <header>
+    <p>MARS'AIR</p>
+    <ul>
+      <li><a href="home">Accueil</a></li>
+      <li><a href="map">Carte</a></li>
+      <li><a href="about">A propos</a></li>
+      <li><a href="contact">Contact</a></li>
+      <li><a href="connexion">Connexion</a></li>
+    </ul>
+  </header>
+  <section>
+
+  </section>
 </body>
+
 </html>
