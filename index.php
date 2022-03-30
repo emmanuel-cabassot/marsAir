c07f6ad1b029b05c61fd079fc6db4ef8f9df7492
nadir <nadir.ziane@laplateforme.io>
2022-03-30 13:08:42 +0200

    modification css et ajout d'assets icons

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
     <section class="homeSection">
       <!-- TEXT PRESENTATION -->
       <div class="text">
-        <p class="logo">MARS'AIR</p>
+        <img src="./assets/layouts/logo-mars'air.png" class="logo-marsair1" alt="logo mars'air">
         <p class="lorem">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
           Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme
           assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de 
